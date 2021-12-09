24e8e6046e02addd609148648d81f0d6bdbaede4
Faizal Dar-Sandur <afaizal1987@gmail.com>
2021-12-10 03:41:46 +0800

    UPDATE:
    - Navbar.js: Name of the Application on Navbar
    - App.js: Positioninig of NavBar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
 import Rate from "./component/Rate"
 // import Cardlist from "./component/Cardlist";
+import NavBar from "./layout/NavBar";
 import './App.css';
 
 function App() {
   return (
-    
-    // Container
-    <div className="container">
-      {/* Using Cards */}
-      <div className="card">
-        <h1>The Minimalist Entreprenuer</h1> 
-        <Rate rating={3.8} review={null}/> 
-        <button style={{padding:'0 5px 0 5px'}}>Add Review</button>
+    <body>
+      <NavBar title="Simple Star Rating System"/>
+      <div className="container">
+        {/* Using Cards */}
+        <div className="card">
+          <h1>The Minimalist Entreprenuer</h1> 
+          <Rate rating={3.8} review={null}/> 
+          <button style={{padding:'0 5px 0 5px'}}>Add Review</button>
+        </div>
+        {/* Separate card for Reviews */}
+        <div className="card">  
+          <h2>Reviews</h2>
+          <ul>
+            <li>
+              <Rate rating={3.8} review={'Text 1'}/>  
+            </li>
+            <li>
+              <Rate rating={4} review={'Text 2'}/> 
+            </li>
+            <li>
+              <Rate rating={3} review={'Text 3'}/> 
+            </li>
+          </ul>
+        </div>
+        <div>
+          What's your rating 
+          <Rate rating={0}/>
+          Review
+          Submit
+        </div>
       </div>
-      {/* Separate card for Reviews */}
-      <div className="card">  
-        <h2>Reviews</h2>
-        <ul>
-          <li>
-            <Rate rating={3.8} review={'Text 1'}/>  
-          </li>
-          <li>
-            <Rate rating={4} review={'Text 2'}/> 
-          </li>
-          <li>
-            <Rate rating={3} review={'Text 3'}/> 
-          </li>
-        </ul>
-      </div>
-      <div>
-        What's your rating 
-        <Rate rating={0}/>
-        Review
-        Submit
-      </div>
-    </div>
+    </body>
   );
 }
 
--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
+import {FaStar} from 'react-icons/fa'
 
-const NavBar = ({icon,title}) => {
+const NavBar = ({title}) => {
   return (
     <nav className="navbar bg-primary">
       <h2>
-        <i className={icon} alt="github" /> {title}
+        <FaStar/> {title}
       </h2>
-      <ul>
-        <li> <Link to='/'> Main </Link> </li>
-        <li> <Link to='/about'> About </Link> </li>
-      </ul>
     </nav>
   )
 }
 // IF no props is defined, it will use this default
 NavBar.defaultProps={
-  title: 'Github Props',
-  icon: 'fab fa-github'
+  title: 'Simple Star Rating System',
 };
 
 NavBar.propTypes={
