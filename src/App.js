80d62d943471e0ae2f6e79f212880e7c4c91b8ec
Faizal Dar-Sandur <afaizal1987@gmail.com>
2021-12-10 03:12:52 +0800

    UPDATE:
    - Index.html: Changed the title
    - Rate.js: Formatting for the reviews and rating
    - App.css: A custom CSS
    - App.js: Restructured the flow a llittle

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import Rate from "./component/Rate";
+import Rate from "./component/Rate"
+// import Cardlist from "./component/Cardlist";
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        The Minimalist Entreprenuer 
-        <Rate rating='3.8'/> 
-        add review
+    
+    // Container
+    <div className="container">
+      {/* Using Cards */}
+      <div className="card">
+        <h1>The Minimalist Entreprenuer</h1> 
+        <Rate rating={3.8} review={null}/> 
+        <button style={{padding:'0 5px 0 5px'}}>Add Review</button>
       </div>
-      <div>  
+      {/* Separate card for Reviews */}
+      <div className="card">  
+        <h2>Reviews</h2>
         <ul>
           <li>
-            <Rate rating='4'/>  book was full of fluff
+            <Rate rating={3.8} review={'Text 1'}/>  
           </li>
           <li>
-            <Rate rating='3'/> book was full of fluff
+            <Rate rating={4} review={'Text 2'}/> 
           </li>
           <li>
-            <Rate rating='4'/> book was full of fluff
+            <Rate rating={3} review={'Text 3'}/> 
           </li>
         </ul>
       </div>
       <div>
         What's your rating 
-        <Rate rating='0'/>
+        <Rate rating={0}/>
         Review
         Submit
       </div>
-
     </div>
   );
 }
--- src/component/Rate.js
+++ src/component/Rate.js
@@ -1,13 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FaStar} from 'react-icons/fa'
+import '../Star.css';
 
-const Rate = ({ rating }) => {
-    return <div>{rating} star rating</div>;
+/**
+ * @params rating number for rates
+ * @todo ADD static identifier to change from changeble to static
+ */
+const Rate = ({ rating, review }) => {
+    
+    // Initialising the state value
+    const [rate, setRate] = useState(rating)
+    const [hover, setHover] = useState(null)
+
+    if(rate === 0){
+        setRate(null)
+    }
+
+    /**
+     * Placing notes here:
+     * 1. For onClick, the function is in the return itself because it is light
+     * 2. We can turn it into a proper function if the application grows bigger
+     *  @todo: Change the setRate function into a proper function
+     */
+
+    // We can capture the rate
+
+    
+    // Returning stars with rating
+    return(
+        <div>
+            {[ ... Array(5)].map((star,i) =>{
+                const ratingValue = i+1;
+                return (
+                    // Labels to group with input
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="rating" 
+                            value={ratingValue}
+                            onClick={()=>setRate(ratingValue)} 
+                        />
+                        <FaStar 
+                            className="star" 
+                            size={15}
+                            color={
+                                ratingValue <= (rate || hover) ? '#ffc107': '#e4e5e9'}
+                            onMouseEnter={()=>setHover(ratingValue)}
+                            onMouseLeave={()=>setHover(null)}
+                        />
+                    </label>
+                )
+            })}
+            {review !== null
+            ?<span style={{fontSize: '1em',paddingLeft:'10px'}}>{rate}, {review}</span>
+            :<span style={{fontSize: '1em',paddingLeft:'10px'}}>{rate}</span>}
+            
+        </div>
+    )
+        
 };
 
+// Initialised the Property Types
 Rate.propTypes = {
   rating: PropTypes.number,
+  review: PropTypes.string
 };
 
 export default Rate;
